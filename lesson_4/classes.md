af60f6566bab32169210fa08d7dd39c2e78df61c
Valentijn Harmers <valentijn.harmers@outlook.com>
2020-02-08 21:29:32 +0100

    Added an Example to the Classess lesson
    Classes.py is not fully documented not but this will do for now.

--- lesson_4/classes.md
+++ lesson_4/classes.md
@@ -21,3 +21,35 @@ class Counter:
         return self._count
 
 ```
+
+## An example
+
+Lets take a look at an example. We want to create a program where we can make two types of vehicles:
+a car and a motorcycle. We want to be able to take our vehicle to the tank station to fill up the tank.
+We also want to be able to take our vehicle to the garage where they will replace the tires if they are
+to worn off.
+
+Lets break apart the story above:
+
+* We have a Vehicle 
+  * A vehicle can be a car
+    * A car has doors
+  * A vehicle can be a motorcycle
+  * A vehicle has an engine
+    * An engine needs fuel to operate
+  * A vehicle needs tires to be able to move
+    * Tires can get worn out
+* The fuel tank of a vehicle can be filled at a fuelstation
+  * A fuelstation can offer different types of fuel
+* Worn tires can be replaced at a garage
+
+De next thing we are going to do is to create an UML. UML stands for Unified Modeling Language.
+An UML can be used to create a high level blueprint of our future Python program. You will
+need a separate piece of software to create an UML diagram. I recommend 
+[Voilet UML](http://alexdp.free.fr/violetumleditor/page.php) but you are free to use whichever
+program you like.
+
+Our first model will look like [this](classes_uml.class.violet.html). This model clearly shows
+the connections between the different classes. The Motorcycle and Car classes are two types
+of vehicle and a vehicle need Tires and an Engine to function. The car class also needs
+doors. De Garage and Fuelstation class work with the vehicle class.
--- lesson_4/classes.py
+++ lesson_4/classes.py
@@ -1,17 +1,34 @@
 from enum import Enum
 
 class Garage:
+    '''
+    This class represents a Garage. The garage
+    is able to replace the tires of Vehicles but
+    can only do this if it is supplied with tires.
+    '''
 
     def __init__(self):
+        '''
+        Creates a new Garage with an empty stock.
+        '''
         self._stock = []
     
     def add_tire(self, tire):
+        '''
+        Add a tire to the stock. The garage will accept
+        used tires but not tires which are to worn out.
+        '''
         if tire.needs_replacement():
             raise RuntimeError("This tire is already too worn out")
         else:
             self._stock.append(tire)
 
     def service_vehicle(self, vehicle):
+        '''
+        Replace any tires which need to be replaced.
+        This function will return the number of
+        tires which where actually replaced.
+        '''
         tires_replaced = 0
 
         while vehicle.has_worn_tires():
@@ -22,18 +39,35 @@ class Garage:
 
 
 class FuelType(Enum):
+    '''
+    The enum represents the different types
+    of fuel a vehicle can have.  
+    '''
     PETROL = 1
     DIESEL = 2
 
 class FuelStation:
 
     def __init__(self):
+        '''
+        Creates a new Fuelstation with an empty fuel supply.
+        '''
         self._fuel = {}
     
     def has_fuel(self, fuel_type):
+        '''
+        Indicates it this station has any fuel of the
+        given fuel type.
+        '''
         return self._fuel.get(fuel_type, 0) > 0
 
     def _retrieve(self, fuel_type, amount):
+        '''
+        Retrieve a certain amount of fuel from
+        this station. This function should
+        only be used by functions who are part of
+        this class.
+        '''
         if self.has_fuel(fuel_type):
             amount_stored = self._fuel[fuel_type]
             if amount_stored - amount < 0:
@@ -44,6 +78,9 @@ class FuelStation:
             raise RuntimeError("No fuel of this type")
 
     def add_fuel(self, fuel_type, amount):
+        '''
+        Add a certain amount of fuel to this station.
+        '''
         if amount <= 0:
             raise ValueError("You can only add fuel and not take it")
 
@@ -51,6 +88,9 @@ class FuelStation:
         self._fuel[fuel_type] = stored_amount + amount
     
     def fill_tank(self, fuel_type, vehicle):
+        '''
+        Fill the tank of the given vehicle with the given type of fuel.
+        '''
         if vehicle.needs_fuel():
             fuel_needed = vehicle.amount_needed()
             self._retrieve(fuel_type, fuel_needed)
@@ -59,48 +99,89 @@ class FuelStation:
             raise RuntimeError("This vehicle has a full tank")
     
 class Door:
+    '''
+    The class represents a door which can be closed of opened.
+    '''
 
     def __init__(self):
+        '''
+        Create a new door which is closed by default.
+        '''
         self._is_closed = True
     
     def open(self):
+        '''
+        Open the door.
+        '''
         if self.is_closed():
             self._is_closed = False
         else:
             raise RuntimeError("Door is already open")
 
     def is_open(self):
+        '''
+        Indicates whenever the door is open.
+        '''
         return not self._is_closed
 
     def close(self):
+        '''
+        Close the door.
+        '''
         if self.is_open():
             self._is_closed = True
         else:
             raise RuntimeError("Door is already closed")
 
     def is_closed(self):
+        '''
+        Indicates whenever the door is closed.
+        '''
         return self._is_closed
 
 class Tire:
+    '''
+    This class represents a vehicle tire.
+    '''
 
     def __init__(self, wear_level=46000):
+        '''
+        Create a new tire. You can optionally
+        configure a wear level.
+        '''
         if wear_level <= 0:
             raise ValueError("Wear level must be a positive value")
         self._wear_level = wear_level
     
     def distance_before_worn(self):
+        '''
+        Returns the distance which this
+        tire can travel before it is totally
+        worn out.
+        '''
         return self._wear_level
-    
-    def is_worn_out(self):
-        return self._wear_level <= 0
-    
+      
     def needs_replacement(self):
+        '''
+        Indicates if this tire need to be
+        replaced. A tire needs to be
+        replaced when it's wear level is
+        lower than 500.
+        '''
         return self._wear_level < 500
 
     def will_be_worn_out(self, distance):
+        '''
+        Indicates whenever this tire will be worn out
+        after traveling the given amount of distance.
+        '''
         return self._wear_level - distance < 0
 
     def register_wear(self, distance):
+        '''
+        Register wear on this tire. The given distance
+        will be subtracted from the wear level.
+        '''
         if distance < 1:
             raise ValueError("Your can't travel negative distances")
         elif self.will_be_worn_out(distance):
@@ -249,7 +330,7 @@ def main():
     my_motor = Motorcycle(400)
 
     station.fill_tank(FuelType.DIESEL, my_car)
-    print("I have filled the tank op my car. Ready to drive!")
+    print("I have filled the tank of my car. Ready to drive!")
     my_car.drive(355)
     print("I have driven a distance of 355 with my car")
     station.fill_tank(FuelType.PETROL, my_motor)
